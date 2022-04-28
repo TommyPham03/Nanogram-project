2ad7c942124b6b97a86f2ff82492b764dc5f935b
Tommy Tran Pham <tommy.t.pham-1@ou.edu>
2022-04-28 15:23:10 -0500

    Copy Project into model class

--- src/edu/ou/cs2334/project5/models/NonogramModel.java
+++ src/edu/ou/cs2334/project5/models/NonogramModel.java
@@ -72,26 +72,157 @@ public class NonogramModel {
 		return cellStates[rowIdx][colIdx];
 	}
 	
+	public boolean getCellStateAsBoolean(int rowIdx, int colIdx) {
+		//get the cell state
+		CellState state = getCellState(rowIdx, colIdx);
+		//turn it into a boolean and return
+		return CellState.toBoolean(state);
+	}
+	
 	public boolean setCellState(int rowIdx, int colIdx, CellState state) {
 		
 	}
 	
-	public void setCellState(int rowIdx, int colIdx, CellState state) {
-		
-	}
-	
 	public int [][] getRowClues(){
+		//return a deep copy of row clues
 		return deepCopy(rowClues);
 	}
 	
 	public int [][] getColClues(){
+		//return a deep copy of col clues
 		return deepCopy(colClues);
 	}
 	
-	public 
-
-
+	public int [] getRowClue(int rowIdx){
+		//return a copy
+		return Arrays.copyOf(rowClues[rowIdx], rowClues[rowIdx].length);
+	}
 	
+	public int [] getColClue(int colIdx){
+		//return a copy
+		return Arrays.copyOf(colClues[colIdx], colClues[colIdx].length);
+	}
+	
+	public boolean isRowSolved(int rowIdx) {
+		return Arrays.equals(rowClues[rowIdx], projectCellStatesRow(rowIdx))
+	}
+	
+	public boolean isColSolved(int colIdx) {
+		return Arrays.equals(colClues[colIdx], projectCellStatesCol(colIdx))
+	}
+	
+	public boolean isSolved() {
+		//make a boolean value to set to true or false
+		boolean solve = true;
+		//for loops to go through the array to see if any is false
+		for(int index = 0; index < getNumRows(); ++index) {
+			if(isRowSolved(index) == false) {
+				solve = false;
+			}
+		}
+		
+		for(int index = 0; index < getNumCols(); ++index) {
+			if(isColSolved(index) == false) {
+				solve = false;
+			}
+		}
+		
+		//if puzzle is solved return false
+		if(solve == false) {
+			return false;
+		}
+		// else return true
+		return false;
+	}
+
+	public void resetCells() {
+		//for loop to go through all the cell states and set them to false
+		for(int index = 0; index < cellStates.length; ++index) {
+			cellStates[index] = CellState.EMPTY;
+		}
+	}
+	
+	/**
+	 * This method is used to project an array of row or columns
+	 * 
+	 * @param cells cell array
+	 * @return return the nonogram numbers of the given array of cell states
+	 */
+	public static List<Integer> project(boolean [] cells){
+		//make a new list to return
+		List<Integer> list = new ArrayList<Integer>();
+		//make a number variable to add to the list
+		int num = 0; 
+		
+		//make a for loop to go through the cells length
+		for(int index = 0; index < cells.length; ++index) {
+			//if the variable at the index is true then increment the count
+			if(cells[index] == true) {
+				++num;
+			}
+			//if it is false then then add the counter in and reset the counter to 0
+			else if(cells[index] == false) {
+				//only adds in if the counter is greater than 0
+				//else there would be a list of 0s 
+				if(num > 0) {
+				list.add(num);
+				num = 0;
+				}
+			}
+		}
+		//add in the last true variable if there is one
+		if(num != 0) {
+			list.add(num);
+		}
+		//if nothing gets add then add in a 0 to show all false
+		if(list.size() == 0) {
+			list.add(0);
+		}
+		//return the list
+		return list;
+	}
+	/**
+	 * This method is used to project the row
+	 * 
+	 * @param rowIdx the given row index
+	 * @return return the projection of the row at the given index
+	 */
+	public List<Integer> projectRow (int rowIdx){
+		int numCols = colClues.length;
+		//make a array with the instance variable
+		boolean [] array = new boolean[numCols];
+		//loop through it 
+		for(int index = 0; index < numCols; ++index) {
+			//set the index of the array to the cell state at that index
+			array[index] = getCell(rowIdx, index);
+		}
+		
+		//call the project method to get the projection and return
+		return project(array);
+	}
+	
+	/**
+	 * This method is used to project the column
+	 * 
+	 * @param colIdx the given column index
+	 * @return return the projection of the column with the given index
+	 */
+	public List<Integer> projectCol(int colIdx){
+		int numRows = rowClues.length;
+		//make a array with the instance variable
+				boolean [] array = new boolean[numRows];
+				//loop through it 
+				for(int index = 0; index < numRows; ++index) {
+					//set the index of the array to the cell state at that index
+					array[index] = getCell(index, colIdx);
+				}
+				
+				//call the project method to get the projection and return
+				return project(array);
+	}
+	
+	
+
 	// This is implemented for you
 	private static CellState[][] initCellStates(int numRows, int numCols) {
 		// Create a 2D array to store numRows * numCols elements
