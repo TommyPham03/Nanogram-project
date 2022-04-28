842730761c6fa9cb1d20aa7aa274efc5e52622df
Tommy Tran Pham <tommy.t.pham-1@ou.edu>
2022-04-28 12:55:28 -0500

    Add in instance variables and toBoolean method to CellState class

--- src/edu/ou/cs2334/project5/models/CellState.java
+++ src/edu/ou/cs2334/project5/models/CellState.java
@@ -2,4 +2,11 @@ package edu.ou.cs2334.project5.models;
 
 public class CellState {
 
+	CellState EMPTY;
+	CellState FILLED;
+	CellState MARKED;
+	
+	public CellState toBoolean(CellState state) {
+		return state;
+	}
 }
