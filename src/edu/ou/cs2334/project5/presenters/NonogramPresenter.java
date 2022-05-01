712ce6480cac59b99d802328b1157dcd448fd4a8
Tommy Tran Pham <tommy.t.pham-1@ou.edu>
2022-05-01 13:44:56 -0500

    Made skeleton for presenter class

--- src/edu/ou/cs2334/project5/presenters/NonogramPresenter.java
+++ src/edu/ou/cs2334/project5/presenters/NonogramPresenter.java
@@ -1,5 +1,10 @@
 package edu.ou.cs2334.project5.presenters;
 
+import java.io.File;
+
+import edu.ou.cs2334.project5.models.CellState;
+import edu.ou.cs2334.project5.models.NonogramModel;
+
 /**
  * 
  * @author Tommy Pham
@@ -7,4 +12,67 @@ package edu.ou.cs2334.project5.presenters;
  */
 public class NonogramPresenter {
 	
+	private NonogramView view;
+	private NonogramModel model;
+	private int cellLength;
+	
+	public NonogramPresenter(int cellLength) {
+		
+	}
+	
+	public void initializePresenter() {
+		
+	}
+	
+	public void initializeView() {
+		
+	}
+	
+	public void bindCellViews() {
+		
+	}
+	
+	public void handleLeftClick(int rowIdx, int colIdx) {
+		
+	}
+	
+	public void handleRightClick(int rowIdx, int colIdx) {
+		
+	}
+	
+	public void updateCellState(int rowIdx, int colIdx, CellState state) {
+		
+	}
+	
+	public void synchronize() {
+		
+	}
+	
+	public void processVictory(){
+		
+	}
+	
+	public void removeCellViewMarks() {
+		
+	}
+	
+	public void configureButtons() {
+		
+	}
+	
+	public void resetPuzzle() {
+		
+	}
+	
+	public void getPane() {
+		
+	}
+	
+	public void getWindow() {
+		
+	}
+	
+	public void open(File file) {
+		
+	}
 }
