33360d9256ab1a47fb0a7aac58385dbc83bc49e6
Tommy Tran Pham <tommy.t.pham-1@ou.edu>
2022-05-01 14:48:41 -0500

    Create skeleton for CellGridView class

---
+++ src/edu/ou/cs2334/project5/views/CellGridView.java
@@ -0,0 +1,24 @@
+package edu.ou.cs2334.project5.views;
+
+import edu.ou.cs2334.project5.models.CellState;
+
+public class CellGridView {
+	private static final String STYLE_CLASS = "cell-grid-view";
+	private CellView [][] cellViews;
+	
+	public CellGridView(int numRows, int numCols, int cellLength) {
+		
+	}
+	
+	public void initCells(int numRows, int numCols, int cellLength) {
+		
+	}
+	
+	public CellView getCellView(int rowIdx, int colIdx) {
+		return null;
+	}
+	
+	public void setCellState(int rowIdx, int colidx, CellState state) {
+		
+	}
+}
