0d4fd675f05398285026bf1841388cbccffbbd5b
Tommy Tran Pham <tommy.t.pham-1@ou.edu>
2022-05-03 16:02:28 -0500

    Add javadoc comments to Main class

--- src/edu/ou/cs2334/project5/Main.java
+++ src/edu/ou/cs2334/project5/Main.java
@@ -7,15 +7,30 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * main class
+ * @author Tommy Pham
+ *
+ */
 public class Main extends Application {
 	private static int IDX_CELL_SIZE = 0;
 	private int DEFAULT_CELL_SIZE = 30;
 	
+	/**
+	 * main constructor 
+	 * 
+	 * @param args string array
+	 */
 	public void main(String [] args) {
 		launch(args);
 		
 	}
 	
+	/**
+	 * start method
+	 * 
+	 * @param primaryStage the stage of JavaFX
+	 */
 	public void start(Stage primaryStage) throws IOException {
 		int defaults = Integer.parseInt(getParameters().getUnnamed().get(DEFAULT_CELL_SIZE));
 		int cell = Integer.parseInt(getParameters().getUnnamed().get(IDX_CELL_SIZE));
